c542cc53e48ce807c6078543b4f70696d8e9dc99
YeiNameless <108295244+YeiNameless@users.noreply.github.com>
2022-06-27 12:52:37 -0500

    css

---
+++ Mascotas_detalle.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>YOURGETPET</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Ionicons -->
+    <link href="css/ionicons.min.css" rel="stylesheet">
+    <link href="css/linear-icon.css" rel="stylesheet">
+    <!-- Owl-carousel -->
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <!-- Magnific popup style sheet -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Dolphin style sheet -->
+    <link rel="stylesheet" href="css/weblysleek-ui-fonts.css">
+    <link rel="stylesheet" href="css/main.css">
+
+
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,700,900" rel="stylesheet">
+  </head>
+  <body>
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="images/mascotass.jpg" alt="">
+            <span class="text--bold">YOURGETPET</span>
+          </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#home">Mascotas <span class="sr-only">(current)</span></a></li>
+            <li><a href="#index">Inicio</a></li>
+            <li><a href="#features">Nosotros</a></li>
+            <li><a href="#price">Tienda</a></li>
+            <li><a href="#clients">Reportar Abuso</a></li>
+            <li><a href="#" class="btn btn-icon btn-round"><span class="btn-icon-container"><i class="lnr lnr-download"></i></span></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+     <section>
+      <div class="fvideo">
+          <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+                <source src="video/perrito.mp4" type="video/mp4">
+          </video>
+     </div> 
+    
+     <div class="contenido">
+       <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-0">
+            <br><br><br><br><br><br><br><br><br>
+              <div class="colores"><h2>Bienvenido al lugar en el que podrás encontrar a tu amigo peludo.</h2></div>
+              <p class="colores">Más que una mascota, será tu compañero de la vida.</p>
+              <div class="movement"><a class="btn btn-default btn-theme btn-rounded-corner" href="#" role="button">Conoce a tu futuro amigo</a>
+              </div>
+          </div>
+        </div>
+       </div>
+     </div>
+ </section>
+ <section id="workflow" class="section padding-top-bottom-65">
+    <div class="col-md-6 col-sm-12 no--padding">
+        <ul class="imagespg">
+            <img src="images/angola.jpg">
+        </ul>
+    </div>
+    <div>    
+        <ul class="col-12 col-md-6 col-sm-12 no--padding">
+            <lu>
+                <h1>
+                   Nombre: Kamto
+                </h1>
+            </lu>
+            <lu>
+                <h2>
+                   Raza: Angola
+                </h2>
+            </lu>     
+            <lu>
+                <p>
+                 Características: Masculino, heterocromía, pelaje blanco, ambundante pelaje.  
+                </p>          
+            </lu> 
+            <lu>
+                <p>
+                Descripción: Es muy hiperactivo cuando agarra confianza, le gustan los juguetes adecuados para sus dientes y que lo peinen.
+                </p>
+            </lu>
+        </ul>
+    </div> 
+    <div>
+        <lu class="col-6 col-md-3 col-sm-6 no--padding">
+            <div class="input-group-addon subscribe-addons">Favoritos</div>  
+        </lu>
+        <lu class="col-6 col-md-3 col-sm-6 no--padding">
+            <div class="input-group-addon subscribe-addons">Adoptar</div>  
+        </lu>
+    
+    </div> 
+    <ul class="col-12 col-md-6 col-sm-12 no--padding">
+        <ul>
+            <h4>
+            Otras fotos con respecto a la raza
+            </h4>
+        </ul>
+        <ul>
+          <div class="col-4 col-md-2 col-sm-4 no--padding">
+          <lu class="imglol">
+            <img src="images/angola1.jpg">
+           </lu>  
+           </div>
+        </ul>
+        <ul>
+          <div class="col-4 col-md-2 col-sm-4 no--padding">
+          <lu class="imglol">
+            <img src="images/angola2.jpg">
+          </lu>
+          </div>  
+        </ul>
+        <ul>
+            <div class="col-4 col-md-2 col-sm-4 no--padding">
+            <lu class="imglol">
+              <img src="images/angola4.jpg">
+            </lu>
+            </div>
+        </ul>
+     </ul>
+    <div class="col-12 col-md-6 col-sm-12 no--padding">
+        <h4>¡Escríbenos!</h4>   
+        <input type="email" class="form-control" id="exampleInputAmount" placeholder="Correo electrónico">
+        <div class="col-md-6">
+            <div class="row">
+                <form action="" class="form_comentario"><textarea name="" id="" placeholder="Escribe algo..."></textarea></form>
+            </div>
+        </div>
+         <div class="input-group-addon subscribe-addons">Enviar</div>
+    </div>     
+ </ul> 
+ </section>
+ <section>
+    <p>
+        <h1>
+            ¡Videos acerca de la raza!
+        </h1>
+    </p>
+   <div>
+      <video src="video/angola.mp4" ></video>
+   </div>
+</section>  
+
+
+
+
+
+ <footer>
+      <section class="section footer-top">
+        <div class="container">
+          <div class="row">
+             <div class="icono"><img src="images/footer.jpg"></div>
+            <div class="col-md-7">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="list-item-header">
+                    contáctanos
+                  </div>
+                  <div class="contact-details">
+                    <div class="media">
+                      <div class="media-left contact-media-left">
+                        <p>Dirección</p>
+                      </div>
+                      <div class="media-body">
+                        <p>560 Judah St &amp; 15th Ave, Apt 5 San Francisco, CA, 230903 Newyork</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <div class="media-left contact-media-left">
+                        <p>Teléfono</p>
+                      </div>
+                      <div class="media-body">
+                        <p>+300 231 0391 </p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <div class="media-left contact-media-left">
+                        <p>Teléfono fijo</p>
+                      </div>
+                      <div class="media-body">
+                        <p>+440 231 258 12</p>
+                      </div>
+                    </div>
+                    <div class="media">
+                      <div class="media-left contact-media-left">
+                        <p>Correo</p>
+                      </div>
+                      <div class="media-body">
+                        <p>yourgetpet@mail.com</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                  <div class="list-item-header">
+                    NUESTRAS REDES
+                  </div>
+                  <div class="list-group footer-list-group">
+                    <a href="#" class="list-group-item">Facebook</a>
+                    <a href="#" class="list-group-item">Twitter</a>
+                    <a href="#" class="list-group-item">Instagram</a> 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="row">
+                <div class="col-md-12 col-sm-0">
+                  <div class="list-item-header">
+                    canales de denuncia al matrato animal.
+                  </div>
+                  <div class="list-group footer-list-group">
+                    <p class="list-group-item">Comunícate a las líneas de atención: 305 398 58 53 - 305366 84 24</p>
+                    <a href="https://bogota.gov.co/sdqs/"target=”_blank” title="DENUNCIA AL MALTRATO ANIMAL" class="list-group-item">Ingresa y reporta tu denuncia de maltrato animal a la página web de Bogotá te Escucha (https://bogota.gov.co/sdqs)</a>
+                    <a href="https://www.animalesbog.gov.co/"target=”_blank” title="PROTECCIÓN ANIMAL" class="list-group-item">Escribe tu denuncia, petición, queja o reclamo al correo electrónico: proteccionanimal@animalesbog.gov.co</a>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+      <section class="footer-bottom">
+        <div class="container">
+          <hr class="footer-devider">
+          <div class="row">
+            <div class="col-md-5"><p class="copyright">Copyright © 2016 Unity Technologies</p></div>
+            <div class="col-md-5">
+              <ul class="footer-menu">
+                <li><a href="">Legal</a></li>
+                <li><a href="">Privacy</a></li>
+                <li><a href="">Policy</a></li>
+                <li><a href="">Cookies</a></li>
+              </ul>
+            </div>
+            <div class="col-md-2">
+              <a id="scroll-top-div" href="" class="pull-right back-top-btn">back to top <span class="btn-icon"><i class="ion-ios-arrow-thin-up"></i></span></a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+    
+    
+    <!-- PRELOADER -->
+    <div class="preloader-body">
+      <div class="cube-wrapper">
+        <div class="cube-folding">
+          <span class="leaf1"></span>
+          <span class="leaf2"></span>
+          <span class="leaf3"></span>
+          <span class="leaf4"></span>
+        </div>
+        <span class="loading" data-name="Loading">Loading</span>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Owl-carousel JS -->
+    <script src="js/owl.carousel.js"></script>
+    <!-- Dolphin JS -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- Dolphin JS -->
+    <script src="js/main.js"></script>  
+   </body>
